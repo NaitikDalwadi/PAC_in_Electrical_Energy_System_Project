803da9003250c7c451589e2144a0f3d1a91e57af
NaitikDalwadi <naitikdalwadi5191@gmail.com>
2025-04-18 20:06:39 +0200

    Multiple Households

--- Weather_API.m
+++ Weather_API.m
@@ -1,2 +1,22 @@
-% Fetch PVGIS data
-[ts_ghi, ts_temp, pv_power] = IRR_DATA_API(40.7128, -74.0060, 2020, 4, 14, 40, 180);
+% Fetch data from Open-Meteo
+url = 'https://api.open-meteo.com/v1/forecast?latitude=51.0261&longitude=7.5647&hourly=direct_radiation&models=icon_seamless&forecast_days=1';
+data = webread(url);
+
+% Extract and preprocess
+times = data.hourly.time;
+radiation = data.hourly.direct_radiation;
+time_dt = datetime(times, 'InputFormat', 'yyyy-MM-dd''T''HH:mm');
+time_seconds = seconds(time_dt - time_dt(1));
+radiation = double(radiation);
+
+% Create timeseries for Simulink
+ts = timeseries(radiation, time_seconds);
+
+% Save to workspace for Simulink
+assignin('base', 'ts_radiation', ts);
+
+% Optional: Plot to verify
+plot(time_seconds, radiation);
+xlabel('Time (seconds)');
+ylabel('Direct Radiation (W/m²)');
+title('Solar Radiation Data');
